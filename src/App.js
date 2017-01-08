d97e008db828b688a36989a6f4aca7aebca373f4
Pei Xiong Liu <pliu@fiksu.com>
2017-01-07 21:12:53 -0500

    Install and hook up firebase for authentication

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 
 class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyBvHUSMRviuYaj0GRLpj8Nc21tKTYWLyYY',
+      authDomain: 'react-native-manager-tutorial.firebaseapp.com',
+      databaseURL: 'https://react-native-manager-tutorial.firebaseio.com',
+      storageBucket: 'react-native-manager-tutorial.appspot.com',
+      messagingSenderId: '627380403097'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
