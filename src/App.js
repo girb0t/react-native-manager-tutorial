7b7d7471ff18d31a518dd53b398f570bc1d519da
Pei Xiong Liu <pliu@fiksu.com>
2017-01-07 21:42:28 -0500

    Create LoginForm scaffolding

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
 
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
   componentWillMount() {
@@ -24,9 +26,7 @@ class App extends Component {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>
-            Hello!
-          </Text>
+          <LoginForm />
         </View>
       </Provider>
     );
