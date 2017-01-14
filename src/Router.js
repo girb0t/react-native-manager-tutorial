5d2ec7f279e378db7b6f1bcb13231195ff5d7211
Pei Xiong Liu <pliu@fiksu.com>
2017-01-14 15:27:32 -0500

    Add EmployeeList and EmployeeList Scene

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     // sceneStyle is global and applies to ALL scenes in application
     <Router sceneStyle={{ marginTop: 350 }}>
       <Scene key="login" component={LoginForm} title="Please Login" />
+      <Scene key="employeeList" component={EmployeeList} title="Employees" />
     </Router>
   );
 };
